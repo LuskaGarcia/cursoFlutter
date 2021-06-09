386f737056f67fc2ccb1eaece7d12b881251369d
LuskaGarcia <lucasflorianogarcia@gmail.com>
2021-06-09 11:19:37 -0300

    Atualização da interface do app
    Utilização do método scaffold

--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -15,15 +15,30 @@ class HomePageState extends State<HomePage> {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(child: GestureDetector(child: Text('Contador: $counter'), 
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+     
+      body:Center(child: GestureDetector(
+        child: Text(
+        'Contador: $counter', 
+      style: TextStyle(fontSize: 20), 
+      ),
         onTap:(){
           setState(() {
              counter++;
           });        
         })),
-
-      );
+         floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+           setState(() {
+             counter++;
+          }); 
+        }
+      ,),
+    );
 
   }
 
